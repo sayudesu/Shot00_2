fc51a8d522ec579eb5383c9b0d207ab03773df76
坂本 雄介 <fko2247032@stu.o-hara.ac.jp>
2022-10-21 11:18:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Object/enemy.cpp
@@ -0,0 +1,40 @@
+#include "DxLib.h"
+#include "game.h"
+#include "enemy.h"
+
+namespace
+{
+
+}
+
+Enemy::Enemy()
+{
+	m_handle = -1;
+}
+
+Enemy::~Enemy()
+{
+	
+}
+
+void Enemy::init()
+{
+	m_pos.x = Game::kScreenWidth - 80.0f;
+	m_pos.y = Game::kScreenHeight / 2;
+	m_vec.x = 0.0f;
+	m_vec.y = 0.0f;
+}
+
+void Enemy::update()
+{
+	m_pos += m_vec;
+}
+
+void Enemy::draw()
+{
+#if false
+	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+#endif
+	//ç∂Çå¸Ç≠
+	DrawTurnGraphF(m_pos.x, m_pos.y, m_handle, true);
+}
